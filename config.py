0e7ca1998a3bb2dcb18a36cefbfe3fb018ece4c6
duqi <13526086892@163.com>
2020-09-30 15:06:13 +0800

    change .env

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "duqi007.."
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_RUL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_RUL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
